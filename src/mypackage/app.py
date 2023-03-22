2016008ac16057d67148419cc9f0d80acef46b7a
shivleelaTalampalli <124252290+shivleelaTalampalli@users.noreply.github.com>
2023-03-22 13:26:24 +0000

    Update app.py

--- src/mypackage/app.py
+++ src/mypackage/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 print("Runing Python script")
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return "Hello, World! CWT"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8000)
